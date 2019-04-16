dc51bdde9fc52929be2a4a57ae16a3e43bd22ab5
JESSICAAMOGUIMBA <jessica.amoguimba@epn.edu.ec>
2019-04-16 14:12:58 -0500

    tabla

---
+++ tabla.php
@@ -0,0 +1,38 @@
+
+<html>
+<head>
+TABLA 
+</head>
+<body> 
+
+<?php
+
+echo "<table border>";
+echo "<tr>";
+  echo "<td><strong>NOMBRE</strong></td>";
+  echo "<td><strong>APELLIDO</strong></td>";
+  echo "<td><strong>EDAD</strong></td>";
+echo "</tr>";
+
+echo  "<tr>";
+  echo "<td>CARLOS</td>";
+  echo "<td>ALMEIDA</td>";
+  echo "<td>32</td>";
+echo "</tr>";
+
+echo "<tr>";
+  echo "<td>JUAN</td>";
+  echo "<td>CORREA</td>";
+  echo "<td>21</td>";
+echo "</tr>";
+
+echo "<tr>";
+  echo "<td>MARIA</td>";
+  echo "<td>MARTINEZ</td>";
+echo "<td>22</td> ";
+echo "</tr>";
+
+echo "</table>";
+?>
+</body>
+</html>
